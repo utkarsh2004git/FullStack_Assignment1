48ab29d9b9af5cf7c7eef532184b847849fc1cd8
ukarsh2004git <utkarshshahare2004@gmail.com>
2024-02-11 21:30:05 +0530

    Requirement 2

---
+++ readFile01.js
@@ -0,0 +1,17 @@
+
+import * as fs from 'fs';
+const fileSystem = fs;
+
+function printFileContents(fileName) {
+
+    fileSystem.readFile(fileName, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading file:', err);
+      return;
+    }
+    console.log('File contents:');
+    console.log(data);
+  });
+}
+
+printFileContents('poem.txt');
--- src/isLeapYear02.js
+++ src/isLeapYear02.js
@@ -1,7 +1,8 @@
 // Importing the entire dateUtil module as a default object
 
 
-import * as dateUtil from './dateUtil.js';
+// import * as dateUtil from './dateUtil.js';
+import dateUtil from './dateUtil.js';
 
 console.log(dateUtil.isLeapYear(2024)); 
 console.log(dateUtil.isLeapYear(2023)); 
